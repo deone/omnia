6098f67c2625245dcff3c0142f339aae6c978322
Oladayo Osikoya <dayo.osikoya@uknglobal.com>
2009-03-25 07:48:50 +0100

    Models altered to suit new use cases

--- omnia/model/models.py
+++ omnia/model/models.py
@@ -11,6 +11,86 @@ from omnia.model import meta
 Base = declarative_base()
 meta.metadata = Base.metadata
 
+class Misc(object):#{{{
+
+    @staticmethod
+    def days():
+        return {
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 7,
+            8: 8,
+            9: 9,
+            10: 10,
+            11: 11,
+            12: 12,
+            13: 13,
+            14: 14,
+            15: 15,
+            16: 16,
+            17: 17,
+            18: 18,
+            19: 19,
+            20: 20,
+            21: 21,
+            22: 22,
+            23: 23,
+            24: 24,
+            25: 25,
+            26: 26,
+            27: 27,
+            28: 28,
+            29: 29,
+            30: 30,
+            31: 31
+        }
+
+    @staticmethod
+    def months():
+        return {
+            1: "January", 
+            2: "February", 
+            3: "March", 
+            4: "April", 
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+
+    @staticmethod
+    def years():
+        return {
+            1990: 1990,
+            1991: 1991,
+            1992: 1992,
+            1993: 1993,
+            1994: 1994,
+            1995: 1995,
+            1996: 1996,
+            1997: 1997,
+            1998: 1998,
+            1999: 1999,
+            2000: 2000,
+            2001: 2001,
+            2002: 2002,
+            2003: 2003,
+            2004: 2004,
+            2005: 2005,
+            2006: 2006,
+            2007: 2007,
+            2008: 2008,
+            2009: 2009
+        }#}}}
+
 class User(Base):#{{{
     __tablename__ = 'user'
     __table_args__ = {'mysql_engine': 'innodb'}
@@ -175,6 +255,7 @@ class ApprovedRequisition(Base):#{{{
     def todict(self):
         return  {
                     "id": self.id,
+                    "requisition_id": self.requisitionid,
                     "requisition": req_dict(self.requisitionid),
                     "user": user_dict(self.userid)
                 }
@@ -189,19 +270,34 @@ class ApprovedRequisition(Base):#{{{
         except Exception, e:
             print_exc()#}}}
 
-class Order(Base):#{{{
-    __tablename__ = 'order'
+class PurchaseOrder(Base):#{{{
+    __tablename__ = 'purchaseorder'
     __table_args__ = {'mysql_engine': 'innodb'}
 
     id = Column(Integer, primary_key=True)
     amount = Column(Integer, nullable=False)
     status = Column(String(50))
     date_created = Column(DateTime, default=datetime.datetime.now())
-    date_closed = Column(DateTime, default=datetime.datetime.now())
-    requisitionid = Column(Integer, ForeignKey('requisition.id', ondelete='RESTRICT', onupdate='CASCADE'), nullable=False)
-    vendorid = Column(Integer, ForeignKey('vendor.id', ondelete='RESTRICT', onupdate='CASCADE'), nullable=False)#}}}
+    date_closed = Column(DateTime, default=datetime.datetime.now())#}}}
 
-class LineItem(Base):
+class Vendor(Base):#{{{
+    __tablename__ = 'vendor'
+    __table_args__ = {'mysql_engine': 'innodb'}
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False, unique=True)
+    address = Column(Text)
+    phone = Column(String(20), nullable=False)
+    
+    @staticmethod
+    def get():
+        return dict([(i.id, i.name) for i in meta.session.query(Vendor)])#}}}
+
+    @staticmethod
+    def get_name_by_id(id):
+        return meta.session.query(Vendor).filter_by(id=id).one().name
+
+class LineItem(Base):#{{{
     __tablename__ = 'lineitem'
     __table_args__ = {'mysql_engine': 'innodb'}
 
@@ -211,18 +307,19 @@ class LineItem(Base):
     specification = Column(Text)
     quantity = Column(Integer, nullable=False)
     unitprice = Column(Integer, nullable=False)
-    vendor = Column(String(100), nullable=False)
+    vendorid = Column(Integer, ForeignKey('vendor.id', ondelete='RESTRICT', onupdate='CASCADE'))
     requisitionid = Column(Integer, ForeignKey('requisition.id', ondelete='RESTRICT', onupdate='CASCADE'))
 
     requisition = relation(Requisition, backref=backref('lineitems', order_by=id))
+    vendor = relation(Vendor, backref=backref('lineitems', order_by=id))
 
-    def __init__(self, name, itemtype, specification, quantity, unitprice, vendor):
+    def __init__(self, name, itemtype, specification, quantity, unitprice, vendorid):
         self.name = name
         self.itemtype = itemtype
         self.specification = specification
         self.quantity = quantity
         self.unitprice = unitprice
-        self.vendor = vendor
+        self.vendorid = vendorid
 
     def todict(self):
         return  {
@@ -232,25 +329,32 @@ class LineItem(Base):
             "specification": self.specification,
             "quantity": self.quantity,
             "unitprice": self.unitprice,
-            "vendor": self.vendor
-        }
+            "vendor": Vendor.get_name_by_id(self.vendorid)
+        }#}}}
 
-class Vendor(Base):
-    __tablename__ = 'vendor'
-    __table_args__ = {'mysql_engine': 'innodb'}
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(200), nullable=False, unique=True)
-    address = Column(Text)
-    phone = Column(String(20), nullable=False)
-
-class Item(Base):
+class Item(Base):#{{{
     __tablename__ = 'item'
     __table_args__ = {'mysql_engine': 'innodb'}
 
     id = Column(Integer, primary_key=True)
     name = Column(String(200), nullable=False, unique=True)
     type = Column(String(100), nullable=False)
-    vendorid = Column(Integer, ForeignKey('vendor.id', ondelete='RESTRICT', onupdate='CASCADE'))
 
-    vendor = relation(Vendor, backref=backref('items', order_by=id))
+    def todict(self):
+        return  {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+        }
+
+    @staticmethod
+    def get():
+        return dict([(i.id, i.name) for i in meta.session.query(Item)]) 
+
+    @staticmethod
+    def get_types():
+        return dict([(i.type, i.type) for i in meta.session.query(Item)])
+
+    @staticmethod
+    def get_by_type(type):
+        return dict([(i.name, i.name) for i in meta.session.query(Item).filter_by(type=type)])#}}}
